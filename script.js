387ae2b73afb2b8facc0fd755e558f805a1019a9
Super Vision <107816707+aARdeLife@users.noreply.github.com>
2023-05-07 00:15:03 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,30 +1,51 @@
-const dial = document.getElementById("dial");
+const dialContainer = document.getElementById('dial-container');
+const dial = document.getElementById('dial');
+const dialMiddle = document.getElementById('dial-middle');
 
-dial.onmousedown = function (event) {
-    event.preventDefault();
+dialMiddle.style.display = 'none'; // Hide the orange dial by default
 
-    let angle = 0;
-    const centerX = dial.getBoundingClientRect().left + dial.offsetWidth / 2;
-    const centerY = dial.getBoundingClientRect().top + dial.offsetHeight / 2;
+let isRotating = false;
+let initialAngle = 0;
+let currentAngle = 0;
 
-    function onMouseMove(event) {
-        const dx = event.clientX - centerX;
-        const dy = event.clientY - centerY;
-        const newAngle = Math.atan2(dy, dx) * (180 / Math.PI) + 90;
+function rotateDial(event) {
+    if (!isRotating) return;
+    const rect = dial.getBoundingClientRect();
+    const centerX = rect.left + rect.width / 2;
+    const centerY = rect.top + rect.height / 2;
+    const angle = Math.atan2(event.clientY - centerY, event.clientX - centerX) * 180 / Math.PI;
+    const delta = initialAngle - angle;
+    dial.style.transform = `rotate(${-delta + currentAngle}deg)`;
+}
 
-        if (newAngle >= 0 && newAngle <= 360) {
-            angle = newAngle;
-            dial.style.transform = `rotate(${angle}deg)`;
-        }
-    }
+dial.addEventListener('mousedown', event => {
+    isRotating = true;
+    const rect = dial.getBoundingClientRect();
+    const centerX = rect.left + rect.width / 2;
+    const centerY = rect.top + rect.height / 2;
+    initialAngle = Math.atan2(event.clientY - centerY, event.clientX - centerX) * 180 / Math.PI;
+});
 
-    document.addEventListener("mousemove", onMouseMove);
-    document.onmouseup = function () {
-        document.removeEventListener("mousemove", onMouseMove);
-        document.onmouseup = null;
-    };
-};
+document.addEventListener('mousemove', rotateDial);
 
-dial.ondragstart = function () {
-    return false;
-};
+document.addEventListener('mouseup', () => {
+    if (!isRotating) return;
+    isRotating = false;
+    currentAngle = (360 + currentAngle - initialAngle) % 360;
+});
+
+dial.addEventListener('dblclick', () => {
+    dialMiddle.style.display = dialMiddle.style.display === 'none' ? 'block' : 'none';
+});
+
+dialMiddle.addEventListener('dblclick', () => {
+    const dialBackground = document.querySelector('.dial-background');
+    dialBackground.style.display = dialBackground.style.display === 'none' ? 'block' : 'none';
+});
+
+dialContainer.addEventListener('click', event => {
+    if (event.target !== dialContainer) return;
+    dialMiddle.style.display = 'none';
+    const dialBackground = document.querySelector('.dial-background');
+    dialBackground.style.display = 'none';
+});
