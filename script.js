8138f501672a1def547afc5e5a9c73b075a096cd
Super Vision <107816707+aARdeLife@users.noreply.github.com>
2023-05-07 19:15:16 +0200

    Update script.js

--- script.js
+++ script.js
@@ -3,37 +3,63 @@ const dial = document.getElementById("dial");
 const dialMiddle = document.getElementById("dial-middle");
 const dialBackground = document.getElementById("dial-background");
 
+dialMiddle.style.display = "none";
+dialBackground.style.display = "none";
+
 let isDragging = false;
 
-function addEventListenersToDial(dialElement) {
-  dialElement.addEventListener("mousedown", (e) => {
-    e.preventDefault();
-    isDragging = true;
-  });
-
-  dialElement.addEventListener("mousemove", (e) => {
-    if (isDragging) {
-      const angle = calculateAngle(e, dialElement);
-      dialElement.style.transform = `rotate(${angle}deg)`;
-    }
-  });
-
-  dialElement.addEventListener("mouseup", () => {
-    isDragging = false;
-    dialElement.style.transition = "transform 0.5s";
-    dialElement.style.transform = `rotate(0deg)`;
-
-    setTimeout(() => {
-      dialElement.style.transition = "none";
-    }, 500);
-  });
+function rotateDial(e, dialElement) {
+  const angle = calculateAngle(e, dialElement);
+  dialElement.style.transform = `rotate(${angle}deg)`;
 }
 
-addEventListenersToDial(dial);
-addEventListenersToDial(dialMiddle);
-addEventListenersToDial(dialBackground);
+function calculateAngle(event, dial) {
+  const rect = dial.getBoundingClientRect();
+  const center = {
+    x: rect.left + rect.width / 2,
+    y: rect.top + rect.height / 2,
+  };
+  const angle = Math.atan2(event.clientY - center.y, event.clientX - center.x);
+  const degree = angle * (180 / Math.PI) + 90;
+  return degree;
+}
 
-dial.addEventListener("dblclick", () => {
+dialContainer.addEventListener("mousedown", (e) => {
+  e.preventDefault();
+  isDragging = true;
+});
+
+dialContainer.addEventListener("mousemove", (e) => {
+  if (isDragging) {
+    rotateDial(e, dial);
+    if (dialMiddle.style.display !== "none") {
+      rotateDial(e, dialMiddle);
+    }
+    if (dialBackground.style.display !== "none") {
+      rotateDial(e, dialBackground);
+    }
+  }
+});
+
+dialContainer.addEventListener("mouseup", () => {
+  isDragging = false;
+  dial.style.transition = "transform 0.5s";
+  dialMiddle.style.transition = "transform 0.5s";
+  dialBackground.style.transition = "transform 0.5s";
+
+  dial.style.transform = `rotate(0deg)`;
+  dialMiddle.style.transform = `rotate(0deg)`;
+  dialBackground.style.transform = `rotate(0deg)`;
+
+  setTimeout(() => {
+    dial.style.transition = "none";
+    dialMiddle.style.transition = "none";
+    dialBackground.style.transition = "none";
+  }, 500);
+});
+
+dial.addEventListener("dblclick", (e) => {
+  e.stopPropagation();
   if (dialMiddle.style.display === "none") {
     dialMiddle.style.display = "block";
   } else {
@@ -50,18 +76,8 @@ dialMiddle.addEventListener("dblclick", (e) => {
   }
 });
 
-dialContainer.addEventListener("click", () => {
+document.addEventListener("click", () => {
   dialMiddle.style.display = "none";
   dialBackground.style.display = "none";
 });
 
-function calculateAngle(event, dial) {
-  const rect = dial.getBoundingClientRect();
-  const center = {
-    x: rect.left + rect.width / 2,
-    y: rect.top + rect.height / 2,
-  };
-  const angle = Math.atan2(event.clientY - center.y, event.clientX - center.x);
-  const degree = angle * (180 / Math.PI) + 90;
-  return degree;
-}
